7f0d7fc241d7aad191b01d84fee6e784c2b6cdfe
Andrew Connington <aconnington@gmail.com>
2015-03-20 23:09:37 -0400

    FileCheck beginnings

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FileCheck::Application.config.secret_key_base = '2c6f00b5d20bcec0b0a02ac7e1c66695c9be3f3e7dc8c53a7c2b70d624cd0a3aaf81a264633cdd4d4baee1c5a44a4558f9a7fd04bc8880a2a41535911c25c172'
